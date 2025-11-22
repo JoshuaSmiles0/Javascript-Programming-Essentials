c21b14bf1843801a3e221f7f19838f706cc90a5c
JoshuaSmiles0 <20108881@mail.wit.ie>
2025-11-22 20:36:30 +0000

    followers count exercise finished

--- jscoursetesting/calculateArea/calculate_area.js
+++ jscoursetesting/calculateArea/calculate_area.js
@@ -9,3 +9,17 @@ function calculateArea () {
  let area = length*width;
   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+function groceryTracker () {
+    input1 = parseFloat(document.getElementById('input1').value);
+    input2 = parseFloat(document.getElementById('input2').value);
+    input3 = parseFloat(document.getElementById('input3').value);
+
+    let totalAmount = input1 + input2 + input3;
+
+    document.getElementById("total").innerHTML = `The total value : ${totalAmount}`
+
+    
+    
+}
